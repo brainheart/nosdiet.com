e10a4c1fab81629b9c6ea0fe0d7968a4382f807e
brainheart <reinhard.engels@gmail.com>
2025-05-06 17:55:22 -0400

    Initial check in. Grabbed pretty much everything from live site with only minimal cleanup.

---
+++ html/test/runway/data.js
@@ -0,0 +1,38 @@
+var records = [
+    {   image: "/images/before_lousy.jpg",
+        title: "Before 2001",
+        subtitle: "~210 pounds"
+    },
+    {   image: "/images/after.jpg",
+        title: "During 2004",
+        subtitle: "~170 pounds"
+    },
+    {   image: "/images/during2005.jpg",
+        title: "During 2005",
+        subtitle: "~170 pounds"
+    },
+    {   image: "/images/2006-during.jpg",
+        title: "During 2006",
+        subtitle: "~170 pounds"
+    },
+    {   image: "/images/2007-during.jpg",
+        title: "During 2007",
+        subtitle: "~170 pounds"
+    },
+    {   image: "/images/2008-during-book.jpg",
+        title: "During 2008",
+        subtitle: "~170 pounds"
+    },
+    {   image: "/images/2008-during-kids.jpg",
+        title: "During 2008 (bonus)",
+        subtitle: "~170 pounds"
+    },
+    {   image: "/images/2009-during.jpg",
+        title: "During 2009",
+        subtitle: "~170 pounds"
+    },
+    {   image: "/images/2010-during.jpg",
+        title: "During 2010",
+        subtitle: "~170 pounds"
+    }
+];
